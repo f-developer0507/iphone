52f82d63a65773381f35e7933c0fcd3f0561c373
f-developer0507 <stormpubg0507@gmail.com>
2024-02-15 20:34:17 +0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const flash = document.querySelector("#flash");
 const record = new webkitSpeechRecognition();
-record.lang = "ru-RU";
 record.start();
+record.lang = "ru-RU";
 
 record.onresult = function (e) {
   const result = e.results[0][0].transcript;
-  if(result == "лю масс") {
-    flash.classList.add("active")
+  if (result == "включи") {
+    flash.classList.add("active");
   }
 };
